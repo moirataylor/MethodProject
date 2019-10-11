f1f99cb644a28cf47e780b697aa17c5dee133f14
moira <moira.taylor.2023@student.lhschools.org>
2019-10-11 09:47:58 -0400

    print hello world

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,18 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+    helloWorld();
+
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
+
+
+
+
+}
+
