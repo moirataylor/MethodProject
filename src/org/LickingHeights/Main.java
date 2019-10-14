da692467fbf9f6b73f1e8d3efb46c836e063363a
moira <moira.taylor.2023@student.lhschools.org>
2019-10-14 09:30:45 -0400

    print hello with user's name

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,18 +1,30 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-    helloWorld();
+        helloWorld();
+        String name;
+       Scanner Keyboard;
+       Keyboard = new Scanner(System.in);
+       System.out.println("What's your name?");
+       name = Keyboard.nextLine();
+       helloName(name);
+
 
     }
 
-    public static void helloWorld(){
+    public static void helloWorld() {
         System.out.println("Hello World");
     }
 
+    public static void helloName(String name) {
+        System.out.println("Hello " + name);
 
 
+    }
 
 }
 
