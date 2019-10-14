983d3b6f8295b85000bcf1e2384e111e32663b25
moira <moira.taylor.2023@student.lhschools.org>
2019-10-14 09:47:28 -0400

    multiply two numbers together

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -7,11 +7,17 @@ public class Main {
     public static void main(String[] args) {
         helloWorld();
         String name;
+        int firstNumber,secondNumber;
        Scanner Keyboard;
        Keyboard = new Scanner(System.in);
        System.out.println("What's your name?");
        name = Keyboard.nextLine();
        helloName(name);
+       int a = 5;
+       int b = 3;
+       System.out.print(multiply(a,b));
+
+
 
 
     }
@@ -22,9 +28,13 @@ public class Main {
 
     public static void helloName(String name) {
         System.out.println("Hello " + name);
-
-
     }
+    public static int multiply(int a, int b ){
+        int answer = a*b;
+        return answer;
+    }
+    
+
 
 }
 
