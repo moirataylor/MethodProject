f86c7f82427ed37cad0309fc3d349fe628d5bc13
moira <moira.taylor.2023@student.lhschools.org>
2019-10-15 09:18:09 -0400

    method for dividing

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         helloWorld();
-        String name;
         int firstNumber,secondNumber;
-       Scanner Keyboard;
-       Keyboard = new Scanner(System.in);
-       System.out.println("What's your name?");
-       name = Keyboard.nextLine();
-       helloName(name);
+       String name;
+        Scanner Keyboard;
+        Keyboard = new Scanner(System.in);
+        System.out.println("name");
+        name = Keyboard.nextLine();
+
        int a = 5;
        int b = 3;
-       System.out.print(multiply(a,b));
-       double x = 
+       System.out.println(multiply(a,b));
+       System.out.println(divide(a,b));
 
 
 
@@ -24,19 +24,23 @@ public class Main {
     }
 
     public static void helloWorld() {
+
         System.out.println("Hello World");
     }
 
     public static String helloName(String name) {
-      return "Hello "+ name;
+      return "Hello " + name;
     }
+
+
     public static int multiply(int a, int b ){
         int answer = a*b;
         return answer;
     }
-    public static double divide(double x, double y){
-        return x/y;
+    public static double divide(double a, double b){
+        return a/b;
     }
+    
 
 
 }
